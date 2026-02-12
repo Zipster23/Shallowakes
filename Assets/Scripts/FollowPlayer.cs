bfa281dfa6b97382c3d3ef832d0c167f9e6bb587
Zipster23 <86753446+Zipster23@users.noreply.github.com>
2026-02-12 14:16:20 -0500

    WIP camera movement

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
-    public Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] private float cameraSensitivity;
 
     private float mouseXInput;
     private float mouseYInput;
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
+        transform.position = player.transform.position + offset;
         mouseXInput = Input.GetAxis("Mouse X");
         mouseYInput = Input.GetAxis("Mouse Y");
-        transform.position = player.transform.position + offset;
+        //use transform.Rotate(-transform.up * rotateHorizontal * sensitivity) instead if you dont want the camera to rotate around the player
+        transform.RotateAround(player.transform.position, Vector3.up, mouseXInput * cameraSensitivity);
+
+        // again, use transform.Rotate(transform.right * rotateVertical * sensitivity) if you don't want the camera to rotate around the player
+        transform.RotateAround(Vector3.zero, transform.right, mouseYInput * cameraSensitivity); 
     }
 }
