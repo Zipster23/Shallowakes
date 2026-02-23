d32b50d18a576bc33712a85a103d50c7a99cdac7
SillySilent <juanpimaldoisaacs@gmail.com>
2026-02-23 09:03:26 -0500

    Long Story Short
    Its Over

--- Assets/KayratMovement/Scripts/PlayerMovement.cs
+++ Assets/KayratMovement/Scripts/PlayerMovement.cs
@@ -11,9 +11,14 @@ public class PlayerMovement : MonoBehaviour
 
     private Transform camTransform;
 
+    private Rigidbody rb;
+
+    private Vector3 moveDir;
+
     private void Awake()
     {
         camTransform = Camera.main.transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     public void Move(Vector2 input)
@@ -41,21 +46,35 @@ public class PlayerMovement : MonoBehaviour
         
         CurrentSpeed = moveDirection.magnitude;
 
-        // if the player has a move direction (is pressing the movement keys)
-        if (moveDirection.magnitude > 0.1f)
+        // do ctrl + / while selecting the block to uncomment
+        // // if the player has a move direction (is pressing the movement keys)
+        // if (moveDirection.magnitude > 0.1f)
+        // {
+        //     // transform the player based on move direction and relative to the world, not the players transform
+        //     // this allows the player to mvoe with respect to camera
+        //     // THIS IS THE LINE TO CHANGE TO Rigidbody.MovePosition to prevent jitters.
+        //     // BUT Antonio figure this part out please.
+        //     // transform.Translate(moveDirection.normalized * speed * Time.deltaTime, Space.World);
+
+        //     rb.AddForce(moveDirection.normalized * speed);
+
+        //     // Gets the direction in which the player is moving
+        //     Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+        //     // Rotates player in that direction
+        //     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        // }
+
+        moveDir = moveDirection;
+    }
+
+    void FixedUpdate()
+    {
+        if (moveDir.magnitude > 0.1f)
         {
-            // transform the player based on move direction and relative to the world, not the players transform
-            // this allows the player to mvoe with respect to camera
-            // THIS IS THE LINE TO CHANGE TO Rigidbody.MovePosition to prevent jitters.
-            // BUT Antonio figure this part out please.
-            transform.Translate(moveDirection.normalized * speed * Time.deltaTime, Space.World);
-
-            // Gets the direction in which the player is moving
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-
-            // Rotates player in that direction
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+            rb.AddForce(moveDir.normalized * speed);
         }
+        
     }
 }
 
