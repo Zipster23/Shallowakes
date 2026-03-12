2903b482b6b9a646e129f170aefb0bd6b88b6e4a
joanasdasdasdasd <joanl8359@gmail.com>
2026-03-12 13:17:41 -0400

    merged with movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -39,19 +39,22 @@ public class PlayerController : MonoBehaviour
     public void Update()
     {
         // move the player based on input
-        movement.Move(input.MovementInput);
+        movement.Move(input.MovementInput, input.sprintInput);
 
         if (input.jumpInput)
         {
             movement.Jump();
         }
 
-        if (input.MovementInput != Vector2.zero)
+        // ADDED CHECK: Only play movement animation if moving AND on the ground
+        if (input.MovementInput != Vector2.zero && movement.isGrounded)
         {
-            controller.PlayMovementAnimation(true);
-        } else
+            controller.PlayMovementAnimation(true, input.sprintInput);
+        }
+        else
         {
-            controller.PlayMovementAnimation(false);
+            // If the player stops moving, OR if they are in the air, stop the run animation
+            controller.PlayMovementAnimation(false, false);
         }
 
         if (input.parryInput)
@@ -69,6 +72,11 @@ public class PlayerController : MonoBehaviour
             {
                 controller.PlayThrustAnimation();
             }
+        }
+
+        if (input.dashInput)
+        {
+            movement.DashOutput();
         }
 
     }
