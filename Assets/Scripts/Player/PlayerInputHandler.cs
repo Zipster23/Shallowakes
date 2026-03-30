6ac7af3ddf17475aad84ac96552b297e51dd46d5
Zipster23 <86753446+Zipster23@users.noreply.github.com>
2026-03-30 08:08:46 -0400

    added multiple color variants of demons

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -32,6 +32,6 @@ public class PlayerInputHandler : MonoBehaviour
         // Sprint Input
         sprintInput = Input.GetKey(KeyCode.LeftShift);
 
-        dashInput = Input.GetKeyDown(KeyCode.RightShift);
+        dashInput = Input.GetKeyDown(KeyCode.Q);
     }
 }
