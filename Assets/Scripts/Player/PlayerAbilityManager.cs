82e567060d726bab4dcd7fcc2bdcd47e439d2db6
Zipster23 <86753446+Zipster23@users.noreply.github.com>
2026-04-09 09:09:49 -0400

    cheatDeath

--- Assets/Scripts/Player/PlayerAbilityManager.cs
+++ Assets/Scripts/Player/PlayerAbilityManager.cs
@@ -19,6 +19,8 @@ public class PlayerAbilityManager : MonoBehaviour
 
     private void Update()
     {
+
+        /*
         // Glide Ability Activation
         if(inputHandler.glideInput && !movement.isGrounded)
         {
@@ -30,6 +32,7 @@ public class PlayerAbilityManager : MonoBehaviour
         {
             
         }
+        */
     }
 
 }
