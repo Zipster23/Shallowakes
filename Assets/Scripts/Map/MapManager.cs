7908cd6e292b66354ab6906676d5806b2819a35f
joanasdasdasdasd <joanl8359@gmail.com>
2026-05-03 09:08:20 -0400

    Lore cutscene and map working now
    just need to improve them

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MapProgress : MonoBehaviour
+public class MapManager : MonoBehaviour
 {
     
     [Header("Stage Sketches")]
