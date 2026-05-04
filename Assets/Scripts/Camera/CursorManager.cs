e4297d51672dad3893e970a931ead747a9eb8434
Zipster23 <86753446+Zipster23@users.noreply.github.com>
2026-05-04 03:18:18 -0400

    finalest changes

--- Assets/Scripts/Camera/CursorManager.cs
+++ Assets/Scripts/Camera/CursorManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class CursorManager : MonoBehaviour
 {
+    private bool _manuallyUnlocked = false;
+
     private void Start()
     {
         LockCursor();
@@ -10,16 +14,53 @@ public class CursorManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Delete))
         {
+            _manuallyUnlocked = true;
             UnlockCursor();
         }
+
+        if (_manuallyUnlocked && Input.GetMouseButtonDown(0))
+        {
+            _manuallyUnlocked = false;
+            LockCursor();
+        }
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus && !_manuallyUnlocked)
+            LockCursor();
+        else if (!hasFocus)
+            UnlockCursor();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        _manuallyUnlocked = false;
+        if (scene.buildIndex == 0)
+            UnlockCursor();
+        else
+            LockCursor();
     }
 
     public void LockCursor()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     public void UnlockCursor()
@@ -28,4 +69,3 @@ public class CursorManager : MonoBehaviour
         Cursor.visible = true;
     }
 }
-
